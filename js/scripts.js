a80d0d41da562c05816eb1b2fc5337e20cea313e
dwd369 <dwd7290@gmail.com>
2023-03-21 20:36:34 -0700

    created pokemonList and added 3 pokemon objects into the array
    Remove existing code for "hello world"
    Created new array pokemonList
    Added pokemon objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
-
-let favoriteFood = "sushi";
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList[0] = {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["Grass","Poison"]
+};
+pokemonList[1] = {
+    name: "Ivysaur",
+    height: 1,
+    types: ["Grass","Poison"]
+};
+pokemonList[2] = {
+    name: "Venusaur",
+    heightInMeters: 2,
+    types: ["Grass","Poison"]
+};
