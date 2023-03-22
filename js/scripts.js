961a4dce8a68bec7575a779c1be47f8df3146aa0
dwd369 <dwd7290@gmail.com>
2023-03-21 23:38:16 -0700

    added function to write pokemon names and height and validate if it is above 1.5 meters in height

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,21 @@ pokemonList[1] = {
 };
 pokemonList[2] = {
     name: "Venusaur",
-    heightInMeters: 2,
+    height: 2,
     types: ["Grass","Poison"]
 };
+
+
+// Loop through pokemonList to add the Pokemon's name and height to index.html
+for (let i = 0; i < pokemonList.length; i++) {
+    // Loop through pokemonList to add the Pokemon's name and height to index.html
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+    
+
+    // Check if the Pokemon's height is above 1.5 meters. If true, indicate that it's a larger Pokemon
+    if (pokemonList[i].height >= 1.5) {
+        document.write(" - Wow, that's big!")
+    }
+
+    document.write("<br/>");
+}
