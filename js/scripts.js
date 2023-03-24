95b15a07feff7c7bd85e153fbf17d184e68782d8
dwd369 <dwd7290@gmail.com>
2023-03-23 19:48:54 -0700

    update pokemonList to adhere to functional programming, added functions for adding a pokemon and a function to return the list of pokemon in the current list

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,53 @@
-let pokemonList = [];
-pokemonList[0] = {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["Grass","Poison"]
-};
-pokemonList[1] = {
-    name: "Ivysaur",
-    height: 1,
-    types: ["Grass","Poison"]
-};
-pokemonList[2] = {
-    name: "Venusaur",
-    height: 2,
-    types: ["Grass","Poison"]
-};
+let pokemonRepository = (function () {
 
+    // declare pokemonList array
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            types: ["Grass","Poison"]
+        },
+        {
+            name: "Ivysaur",
+            height: 1,
+            types: ["Grass","Poison"]
+        },
+        {
+            name: "Venusaur",
+            height: 2,
+            types: ["Grass","Poison"]
+        }
+    ];
 
-// Loop through pokemonList to add the Pokemon's name and height to index.html
-for (let i = 0; i < pokemonList.length; i++) {
-    // Loop through pokemonList to add the Pokemon's name and height to index.html
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
-    
-
-    // Check if the Pokemon's height is above 1.5 meters. If true, indicate that it's a larger Pokemon
-    if (pokemonList[i].height >= 1.5) {
-        document.write(" - Wow, that's big!")
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+        console.log("Pokemon added successfully");
     }
 
-    document.write("<br/>");
-}
+    function getAll() {
+        let text = "";
+        pokemonList.forEach(function(pokemon) {
+            
+            // Loop through pokemonList to add the Pokemon's name and height to index.html
+            text += (`${pokemon.name} (height: ${pokemon.height})`);
+
+            // Check if the Pokemon's height is above 1.5 meters. If true, indicate that it's a larger Pokemon
+            if (pokemon.height >= 1.5) {
+                text += (" - Wow, that's big!")
+            }
+            
+            // Add line break in between
+            text += "<br/>";
+        })
+
+        return text;
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+document.write(pokemonRepository.getAll());
