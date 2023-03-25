2fbf126f3fabc457f644220af615d4f93c03b037
dwd369 <dwd7290@gmail.com>
2023-03-24 21:11:30 -0700

    update getAll method, update add method to validate on inputted pokemon, updated forEach loop to work with new getAll method, added public function for findPokemon

--- js/scripts.js
+++ js/scripts.js
@@ -5,42 +5,42 @@ let pokemonRepository = (function () {
         {
             name: "Bulbasaur",
             height: 0.7,
-            types: ["Grass","Poison"]
+            types: ["Grass"," Poison"]
         },
         {
             name: "Ivysaur",
             height: 1,
-            types: ["Grass","Poison"]
+            types: ["Grass"," Poison"]
         },
         {
             name: "Venusaur",
             height: 2,
-            types: ["Grass","Poison"]
+            types: ["Grass"," Poison"]
         }
     ];
 
     function add(pokemon) {
-        pokemonList.push(pokemon);
-        console.log("Pokemon added successfully");
+
+        // Validation of Pokemon entered by users. 
+        //Validation criteria includes if it is an object, if the keys matched, if name is string, if height is number, and if type is array
+        if (typeof(pokemon) !== "object") {
+            console.log("This is not a valid Pokemon, please try again.");
+        } else if (!(Object.keys(pokemonList[0]).every(key => key in pokemon))) {
+            console.log("Please provide the right keys and values for adding a new pokemon.")
+        } else if (typeof(pokemon.name) !== "string") {
+            console.log("The name for the Pokemon is not a string, please try again.");
+        } else if (typeof(pokemon.height) !== "number") {
+            console.log("The height you entered for the Pokemon is not a number, please try again.");
+        } else if (!(Array.isArray(pokemon.types))) {
+            console.log("The type(s) you entered for the Pokemon is not an array, please try again.");
+        } else {
+            pokemonList.push(pokemon);
+            console.log("Pokemon added successfully");
+        } 
     }
 
     function getAll() {
-        let text = "";
-        pokemonList.forEach(function(pokemon) {
-            
-            // Loop through pokemonList to add the Pokemon's name and height to index.html
-            text += (`${pokemon.name} (height: ${pokemon.height})`);
-
-            // Check if the Pokemon's height is above 1.5 meters. If true, indicate that it's a larger Pokemon
-            if (pokemon.height >= 1.5) {
-                text += (" - Wow, that's big!")
-            }
-            
-            // Add line break in between
-            text += "<br/>";
-        })
-
-        return text;
+        return pokemonList;
     }
 
     return {
@@ -50,4 +50,41 @@ let pokemonRepository = (function () {
 
 })();
 
-document.write(pokemonRepository.getAll());
+function findPokemon(inPokedex, pokemonName) {
+    let pokemons = [];
+    inPokedex.forEach(function(pokemon) {
+        pokemons.push(pokemon.name);
+    });
+
+    let result = pokemons.filter(pokemon => pokemon === pokemonName);
+
+    if (result.length === 0) {
+        return `${pokemonName} does not exist in Pokedex`
+    } else {
+        return `${pokemonName} is in the Pokedex`
+    }
+}
+
+
+// Retrieve pokemonList from pokemonRepository
+let pokedex = pokemonRepository.getAll();
+
+
+// Write Pokedex to DOM body
+pokedex.forEach(function(pokemon) {
+    
+    // Loop through pokemonList to add the Pokemon's name and height to index.html
+    document.write(`${pokemon.name} (height: ${pokemon.height}, type: ${pokemon.types})`);
+
+    // Check if the Pokemon's height is above 1.5 meters. If true, indicate that it's a larger Pokemon
+    if (pokemon.height >= 1.5) {
+        document.write(" - Wow, that's big!");
+    }
+    
+    // Add line break in between
+    document.write("<br/>");
+});
+
+
+let abc = findPokemon(pokedex,"Bulbasaur");
+document.write(abc);
